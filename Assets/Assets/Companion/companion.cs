c9204de3fd5177d73665dc33ed4e3a61033cb583
Sanskar Rathore <sanskarrathore1553+github@gmail.com>
2024-04-26 02:22:31 +0530

    companian and Breath exercise

--- Assets/Assets/Companion/companion.cs
+++ Assets/Assets/Companion/companion.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 
-public class xompanion : MonoBehaviour
+public class companion : MonoBehaviour
 {
+
+    public Transform player;
     public Transform targetPosition;
     public Transform targetPosition2;
     public Transform targetPosition3;
+    public Transform targetPosition4;
     public Animator animator;
-    public float movementSpeed = 2.0f;
-
-    
+    private float movementSpeed = 2.0f;
+    private bool reachedTarget2 = false;
+    private bool reachedTarget3 = false;
+    private bool InteractWithPlayer = false;
+    private bool MoveTowardsPlayer = false;
 
     void Update()
     {
@@ -17,6 +22,7 @@ public class xompanion : MonoBehaviour
             if (waiting_to_MRI.check == 1)
             {
                 animator.SetBool("MoveTowardsPlayer", true);
+                animator.SetBool("InteractWIthPlayer", false);
                 transform.LookAt(targetPosition.position);
                 transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, Time.deltaTime * movementSpeed);
             }
@@ -24,21 +30,55 @@ public class xompanion : MonoBehaviour
             if (Vector3.Distance(transform.position, targetPosition.position) < 0.1f)
             {
                 animator.SetBool("InteractWithPlayer", true);
+                transform.LookAt(player.position);
+                animator.SetBool("MoveTowardsPlayer", false);
+            }
+            else
+            {
+                animator.SetBool("InteractWIthPlayer", false);
+                animator.SetBool("MoveTowardsPlayer", true);
             }
         }
         else
         {
-            animator.SetBool("InteractWithPlayer", false);
-            //animator.SetBool("MoveTowardsPlayer", true);
-            transform.LookAt(targetPosition2.position);
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition2.position, Time.deltaTime * movementSpeed);
-        }
-        if(transform.position == targetPosition2.position)
-        {
-            //animator.SetBool("MoveTowardsPlayer", true);
-            transform.LookAt(targetPosition3.position);
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition3.position, Time.deltaTime * movementSpeed);
+            if (!reachedTarget2)
+            {
+                movementSpeed = 1.5f;
+                animator.SetBool("MoveTowardsPlayer", true);
+                transform.LookAt(targetPosition2.position);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition2.position, Time.deltaTime * movementSpeed);
 
+                if (Vector3.Distance(transform.position, targetPosition2.position) < 0.1f)
+                {
+                    reachedTarget2 = true;
+                    targetPosition2.gameObject.SetActive(false);
+                    targetPosition2.position = targetPosition3.position;
+                }
+            }
+            else if (!reachedTarget3)
+            {
+                movementSpeed = 1.5f;
+                animator.SetBool("MoveTowardsPlayer", true);
+                transform.LookAt(targetPosition3.position);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition3.position, Time.deltaTime * movementSpeed);
+                if (Vector3.Distance(transform.position, targetPosition2.position) < 0.1f)
+                {
+                    reachedTarget3 = true;
+                }
+            }
+            else
+            {
+
+                movementSpeed = 1.5f;
+                animator.SetBool("MoveTowardsPlayer", true);
+                transform.LookAt(targetPosition4.position);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition4.position, Time.deltaTime * movementSpeed);
+
+                if (Vector3.Distance(transform.position, targetPosition4.position) < 0.1f)
+                {
+                    transform.LookAt(player.position);
+                }
+            }
         }
     }
 }
