c25d55d384fc96cfffb68a174d5155e1ac1f62bc
Love Dewangan <lovedewangan9@gmail.com>
2024-04-27 03:09:39 +0530

    MRI Completion

--- Assets/Scripts/MRI_Interaction.cs
+++ Assets/Scripts/MRI_Interaction.cs
@@ -6,8 +6,12 @@ public class MRI_Interaction : MonoBehaviour
 {
     public GameObject targetObject;
     public GameObject targetReached;
+    public GameObject targetExit;
     public GameObject mriUI;
+    public GameObject mriInsideUI;
+    public GameObject mriExitUI;
     public bool activateUpdate = false;
+    public bool mriExitUpdate = false;
     public float moveSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
@@ -38,8 +42,46 @@ public class MRI_Interaction : MonoBehaviour
             }*/
             if (transform.position == targetReached.transform.position)
             {
+                mriInsideUI.gameObject.SetActive(true);
                 activateUpdate = false; // Deactivate the Update function
                 Debug.Log("DeActivated");
+                StartCoroutine(ShowExitUIDelayed());
+            }
+
+            // Calculate the distance to move this frame
+
+
+            // Move the GameObject towards the targetObject
+
+
+            // Check if the GameObject has reached the targetObject
+
+        }
+        if (mriExitUpdate)
+        {
+            Debug.Log("ExitStarted");
+            // Calculate the direction to move towards the targetObject
+            Vector3 direction = (targetObject.transform.position - transform.position).normalized;
+
+            float distanceToMove = moveSpeed * Time.deltaTime * 0.1f;
+            // Move the GameObject towards the targetObject
+            transform.position = Vector3.MoveTowards(transform.position, targetObject.transform.position, distanceToMove);
+
+            // Check if the GameObject is close enough to the targetObject
+            /* if (Vector3.Distance(transform.position, targetObject.transform.position) < 0.1f)w
+            {
+                // If close enough, stop moving and deactivate the Update function
+                transform.position = targetReached.transform.position;
+                activateUpdate = false;
+                Debug.Log("DeActivated");
+            }*/
+            if (transform.position == targetObject.transform.position)
+            {
+                mriExitUpdate = false;
+                Debug.Log("ExitedMRI");
+                Vector3 position = targetExit.transform.position;
+                transform.position = position;
+                transform.rotation = targetExit.transform.rotation;
             }
 
             // Calculate the distance to move this frame
@@ -69,10 +111,26 @@ public class MRI_Interaction : MonoBehaviour
         
 
     }
+
+
     
     public void ActivateMRIMovement()
     {
         activateUpdate = true;
+        mriUI.gameObject.SetActive(false);
     }
     
+    public void ExitMRI()
+    {
+        mriExitUpdate = true;
+        mriExitUI.gameObject.SetActive(false);
+        mriInsideUI.gameObject.SetActive(false);
+    }
+
+    IEnumerator ShowExitUIDelayed()
+    {
+        yield return new WaitForSeconds(2f); // Adjust the delay time as needed
+
+        mriExitUI.SetActive(true);
+    }
 }
