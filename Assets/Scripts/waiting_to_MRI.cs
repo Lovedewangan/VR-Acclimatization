e730f112fceda990f83c94d382a93355fd2f3889
Sanskar Rathore <sanskarrathore1553+github@gmail.com>
2024-04-27 12:40:18 +0530

    door

--- Assets/Scripts/waiting_to_MRI.cs
+++ Assets/Scripts/waiting_to_MRI.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class waiting_to_MRI : MonoBehaviour
 {
     public static int check = 0;
     public GameObject timer_canvas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,7 @@ public class waiting_to_MRI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -23,4 +25,11 @@ public class waiting_to_MRI : MonoBehaviour
         Destroy(gameObject);
         timer_canvas.SetActive(true);
     }
+
+      
+  
+    
+       
+   
+
 }
