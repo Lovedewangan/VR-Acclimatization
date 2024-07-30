4c35be3cee0c9dbda42fa94298ab5e92ed0d64e1
Love Dewangan <lovedewangan9@gmail.com>
2024-07-30 22:47:07 +0530

    Changes

--- Assets/Scripts/ConversationStarter.cs
+++ Assets/Scripts/ConversationStarter.cs
@@ -2,16 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
+using UnityEngine.XR;
 
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-       // if (Input.GetKeyUp(KeyCode.F))
-       // {
+        // Check if the PrimaryIndexTrigger is pressed
+        if (IsPrimaryIndexTriggerPressed())
+        {
             ConversationManager.Instance.StartConversation(myConversation);
-      //  }
+        }
+    }
+
+    private bool IsPrimaryIndexTriggerPressed()
+    {
+        // Get the list of XR devices
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        foreach (var device in devices)
+        {
+            // Check if the device has the PrimaryIndexTrigger
+            if (device.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+            {
+                // Check if the trigger value exceeds a threshold (e.g., 0.5 for half-pressed)
+                if (triggerValue > 0.1f)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
