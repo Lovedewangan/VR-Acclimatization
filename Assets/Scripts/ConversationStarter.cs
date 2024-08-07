1dfef763a644e16512645340a62a9c53d495a73c
Sanskar Rathore <sanskarrathore1553+github@gmail.com>
2024-08-07 19:05:33 +0530

    minor changes

--- Assets/Scripts/ConversationStarter.cs
+++ Assets/Scripts/ConversationStarter.cs
@@ -7,35 +7,48 @@ using UnityEngine.XR;
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
+    [SerializeField] private GameObject convoUIManager;
+    [SerializeField] private AudioSource NPCSound;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         // Check if the PrimaryIndexTrigger is pressed
         //if (IsPrimaryIndexTriggerPressed())
         //{
-            ConversationManager.Instance.StartConversation(myConversation);
+        convoUIManager.SetActive(true);
+        NPCSound.Play();
+        
+        ConversationManager.Instance.StartConversation(myConversation);
         //}
     }
-
-   /* public bool IsPrimaryIndexTriggerPressed()
+    private void OnTriggerExit(Collider other)
     {
-        // Get the list of XR devices
-        List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
+        NPCSound.Stop();
+        //convoUIManager.SetActive(false);
+        Destroy(convoUIManager);
+        Destroy(gameObject);
+    }
 
-        foreach (var device in devices)
-        {
-            // Check if the device has the PrimaryIndexTrigger
-            if (device.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
-            {
-                // Check if the trigger value exceeds a threshold (e.g., 0.5 for half-pressed)
-                if (triggerValue > 0.1f)
-                {
-                    return true;
-                }
-            }
-        }
 
-        return false;
-    }*/
+    /* public bool IsPrimaryIndexTriggerPressed()
+     {
+         // Get the list of XR devices
+         List<InputDevice> devices = new List<InputDevice>();
+         InputDevices.GetDevices(devices);
+
+         foreach (var device in devices)
+         {
+             // Check if the device has the PrimaryIndexTrigger
+             if (device.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+             {
+                 // Check if the trigger value exceeds a threshold (e.g., 0.5 for half-pressed)
+                 if (triggerValue > 0.1f)
+                 {
+                     return true;
+                 }
+             }
+         }
+
+         return false;
+     }*/
 }
